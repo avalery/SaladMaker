e76acb9f04a67a8c0246b0d3b5289b26701cc329
Aurélie <aurelie.valery@gmail.com>
2013-07-13 12:05:58 +0200

    Add project to repository

---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Sm.ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Resources.SaladWillBeGenerated);
+            Console.ReadKey();
+        }
+    }
+}
